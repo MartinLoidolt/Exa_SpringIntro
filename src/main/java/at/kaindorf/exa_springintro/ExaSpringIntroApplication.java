a9210e5644f95ea7d4e5471937b2ac80a20bac30
martin.loidolt <martin.loidolt04@gmail.com>
2022-11-14 11:55:37 +0100

    Initial commit

---
+++ src/main/java/at/kaindorf/exa_springintro/ExaSpringIntroApplication.java
@@ -0,0 +1,13 @@
+package at.kaindorf.exa_springintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaSpringIntroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExaSpringIntroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/kaindorf/exa_springintro/ExaSpringIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package at.kaindorf.exa_springintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaSpringIntroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
